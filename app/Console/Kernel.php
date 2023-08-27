cf0bcfda6038d6141148a0100372074120d24b0a
Roman <romanrus98@mail.ru>
2023-08-27 13:50:13 +0500

    cron test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
+use Illuminate\Support\Facades\Log;
 
 
 class Kernel extends ConsoleKernel
@@ -14,7 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:queue')->everyMinute();
+        // $schedule->command('app:queue')->everyMinute();
+        Log::info("start cron");
     }
 
     /**
